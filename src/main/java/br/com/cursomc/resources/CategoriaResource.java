ce6ece2b3b729cc2d000058e02e180380a7f3937
Adriana Olindo <adriana-olindo18@hotmail.com>
2023-10-09 14:42:53 -0300

    Listando todas categorias com DTO

--- src/main/java/br/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package br.com.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.cursomc.domain.Categoria;
+import br.com.cursomc.dto.CategoriaDTO;
 import br.com.cursomc.services.CategoriaService;
 
 @RestController
@@ -52,6 +55,12 @@ public class CategoriaResource {
 		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
 		
+	}
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> lista = categoriaService.buscarTodos();
+		List<CategoriaDTO> listaDto = lista.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDto);
 	}
 
 }
--- src/main/java/br/com/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/cursomc/services/CategoriaService.java
@@ -1,10 +1,13 @@
 package br.com.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.cursomc.domain.Categoria;
+import br.com.cursomc.dto.CategoriaDTO;
 import br.com.cursomc.repositories.CategoriaRepository;
 import br.com.cursomc.services.exceptions.DataIntegrityException;
 import br.com.cursomc.services.exceptions.ObjectNotFoundException;
@@ -44,6 +47,11 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> buscarTodos(){
+		return categoriaRepository.findAll();
+		
 	}
 
 }
