8057e3b2123b48b7f59b58951d26abf093347fc5
Adriana Olindo <adriana-olindo18@hotmail.com>
2023-10-16 10:41:21 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

--- src/main/java/br/com/cursomc/services/PedidoService.java
+++ src/main/java/br/com/cursomc/services/PedidoService.java
@@ -39,6 +39,9 @@ public class PedidoService {
 	@Autowired
 	private ClienteService clienteService;
 	
+	@Autowired
+	private EmailService emailService;
+	
 
 	public PedidoService(PedidoRepository pedidoRepository) {
 		this.pedidoRepository = pedidoRepository;
@@ -73,7 +76,7 @@ public class PedidoService {
 		}
 		
 		itemPedidoRepository.saveAll(obj.getItens());
-		System.out.println(obj);
+		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
 	
